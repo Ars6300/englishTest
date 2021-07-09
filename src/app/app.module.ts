bd71b3d0f6cd8c8e98c7db8be9e49aef83a7ad93
Vsevolod1020 <vsevolod.gorobec@inbox.ru>
2021-07-09 13:29:08 +0500

    added some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectLevelComponent } from './modules/level-select/level-select.component';
+import { LevelSelectModule } from './modules/level-select/level-select.module';
+import { LeftLayoutComponent } from './shared/components/left-layout/left-layout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectLevelComponent,
+    LeftLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LevelSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/modules/level-select/level-select.module copy.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LevelSelectRoutingModule } from './level-select-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LevelSelectRoutingModule
+  ]
+})
+export class LevelSelectModule { }
