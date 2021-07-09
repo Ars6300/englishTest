2a08a2785aad9ed7975ba61e49b68ab10896a448
zolotou <vadimkarulin@gmail.com>
2021-07-09 13:45:12 +0300

    feat: add language select component and add some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
