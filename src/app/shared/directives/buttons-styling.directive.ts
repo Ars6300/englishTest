14cf9ace5f10c5875de00d6384015c62177a76d7
DobrovskayaElena <lenayalena@tut.by>
2021-07-09 22:51:24 +0300

    style: update button directive

--- src/app/shared/directives/buttons-styling.directive.ts
+++ src/app/shared/directives/buttons-styling.directive.ts
@@ -8,13 +8,16 @@ export type ButtonType = 'primary' | 'default' | 'danger' | null;
 export class ButtonsStylingDirective {
   constructor(private el: ElementRef) {
     let btnType = this.el.nativeElement.attributes.buttontype.nodeValue;
-    if (btnType === 'primary') {
-      this.el.nativeElement.style.backgroundColor = '#7ad2f4';
-    } else if (btnType === 'default') {
-      this.el.nativeElement.style.backgroundColor = 'transparent';
-      this.el.nativeElement.style.color = '#545454';
-    } else if (btnType === 'danger') {
-      this.el.nativeElement.style.backgroundColor = 'red';
+    switch (btnType) {
+      case 'primary': this.setColors('#7ad2f4'); break;
+      case 'danger': this.setColors('red'); break;
+      case 'default':
+      default: this.setColors('transparent', '#545454');
     }
+  }
+
+  setColors(bkgColor: string, color: string = 'initial') {
+    this.el.nativeElement.style.backgroundColor = bkgColor;
+    this.el.nativeElement.style.color = color;
   }
 }
