a9499a81fe7e092971b3124bb57e230d7550544d
DobrovskayaElena <lenayalena@tut.by>
2021-07-12 11:46:56 +0300

    fix: change optional mode

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -1,8 +1,8 @@
 export interface IUser {
-    userId?: string;
-    email?: string;
-    password?: string;
-    name?: string;
-    surname?: string;
-    role?: string;
+    userId: string;
+    email: string;
+    password: string;
+    name: string;
+    surname: string;
+    role: string;
 }
