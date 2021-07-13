5a9f029d3a03617402501f5d2b2221051dbb29d4
DobrovskayaElena <lenayalena@tut.by>
2021-07-13 21:27:57 +0300

    refactor: change models

--- src/app/core/models/questions.model.ts
+++ src/app/core/models/questions.model.ts
@@ -1,18 +1,14 @@
+import { Level } from './level.model';
 export interface Questions {
-  userId: string;
-  role: string;
-  text: string;
   questionId: string;
-  answerId: string;
+  answers: [];
+  text: string;
   type: string;
-  levelId: 'a1' | 'a2' | 'b1' | 'b2' | 'c1' | 'c2';
-  // level: "beginner" | "elementary" | "intermediate" | "upperintermediate" | "advanced" | "proficiency"
+  audioLink: string;
+  level: Level;
 }
 
-export interface Answers {
+export interface Answer {
   questionId: string;
   answerId: string;
-  text: string;
-  isCorrect: boolean;
-  incorrect: [];
 }
