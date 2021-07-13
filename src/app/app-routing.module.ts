feca122da6e9ef9755475252ea98ebef3f498768
Vsevolod1020 <vsevolod.gorobec@inbox.ru>
2021-07-13 18:57:12 +0500

    feat: added-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './modules/error/error.component';
+import { ErrorModule } from './modules/error/error.module'
+
 import { LoginModule } from './modules/login/login.module';
 
 const routes: Routes = [
@@ -13,9 +16,13 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/profile/profile.module').then((m) => m.ProfileModule),
   },
+  {
+    path: 'error',
+    component: ErrorComponent
+  }
 ];
 @NgModule({
-  imports: [LoginModule, RouterModule.forRoot(routes)],
+  imports: [LoginModule, RouterModule.forRoot(routes), ErrorModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { LevelSelectModule } from './modules/level-select/level-select.module';
+import { SelectLevelComponent } from './modules/level-select/level-select.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  declarations: [AppComponent, SelectLevelComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, LevelSelectModule],
   providers: [],
   bootstrap: [AppComponent],
 })
