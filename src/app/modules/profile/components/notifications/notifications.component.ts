68b3214968fad01375aa6e70c57309d5b1cb0779
ars6300 <ars6300@live.com>
2021-07-14 19:48:13 +0500

    feat: fulfill notifications component

--- src/app/modules/profile/components/notifications/notifications.component.ts
+++ src/app/modules/profile/components/notifications/notifications.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Notification } from 'src/app/core/models/notification.model';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
+  constructor(private router: Router) {}
+  notifications: Notification[] = [
+    {
+      title: 'You have test assigned',
+      assignedBy: 'HR',
+      deadline: new Date().toLocaleDateString('en-UK'),
+    },
+  ];
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  goToSelectLevel() {
+    this.router.navigate(['select-level']);
   }
-
 }
