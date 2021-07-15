c852a682a201acff3bd3cec0bf7c3512b88b1d1a
Vsevolod1020 <vsevolod.gorobec@inbox.ru>
2021-07-15 20:58:51 +0500

    feat: added localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,32 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { LevelSelectModule } from './modules/level-select/level-select.module';
 import { QuestionComponent } from './pages/question/question.component';
+import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MissingTranslationService } from './utils';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return new TranslateHttpLoader(http, '../assets/locale/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent, QuestionComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, LevelSelectModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    SharedModule, 
+    LevelSelectModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      missingTranslationHandler: { provide: MissingTranslationHandler, useClass: MissingTranslationService },
+      useDefaultLang: false,
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
