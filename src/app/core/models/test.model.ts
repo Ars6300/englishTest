6970bdc369709401ce0a80fc6c8b8d4cc2d98e2c
DobrovskayaElena <lenayalena@tut.by>
2021-07-22 16:25:06 +0300

    feat: sync local storage questions

--- src/app/core/models/test.model.ts
+++ src/app/core/models/test.model.ts
@@ -1,12 +1,12 @@
 export enum QuestionType {
-    Grammar = 1,
-    Listening = 2,
-    Writing = 3,
-    Speaking = 4,
+    Grammar = '1',
+    Listening = '2',
+    Writing = '3',
+    Speaking = '4',
 }
 
 export interface Tests {
-  type: number;
+  type: QuestionType;
   text: string;
   answers: [{ id: string; answer: string }];
 }
