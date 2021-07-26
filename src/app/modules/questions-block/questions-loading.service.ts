1962bcab7ff3b88841f6ff86b8d3c33eda238db0
DobrovskayaElena <lenayalena@tut.by>
2021-07-26 22:41:55 +0300

    feat: some crud operations created

--- src/app/modules/questions-block/questions-loading.service.ts
+++ src/app/modules/questions-block/questions-loading.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Answer, Question } from 'src/app/core/models/questions.model';
 import { QUESTIONS_MOCK } from 'src/app/redux/reducers/questions.reducers';
@@ -35,7 +36,35 @@ export class QuestionsLoadingService {
     );
   }
 
-  audioTryCount() {
-    
+  postQuestion(data: any) {
+    return this.http.post<any>('http://localhost:3000/posts', data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  getQuestion() {
+    return this.http.get<any>('http://localhost:3000/posts').pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  updateQuestion(data: any, id: string) {
+    return this.http.put<any>('http://localhost:3000/posts' + id, data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  deleteQuestion(id: string) {
+    return this.http.delete<any>('http://localhost:3000/posts' + id).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
   }
 }
--- src/app/pages/questions-edit/questions-table/questions-table.component.ts
+++ src/app/pages/questions-edit/questions-table/questions-table.component.ts
@@ -9,24 +9,35 @@ import { QuestionsLoadingService } from 'src/app/modules/questions-block/questio
 import { QuestionsState } from 'src/app/redux/reducers/questions.reducers';
 import { getQuestions } from 'src/app/redux/selectors/questions.selectors';
 
+export class QuestionModel {
+  id: string = '';
+  text: string = '';
+  type: string = '';
+}
 @Component({
   selector: 'app-questions-table',
   templateUrl: './questions-table.component.html',
-  styleUrls: ['./questions-table.component.scss']
+  styleUrls: ['./questions-table.component.scss'],
 })
 export class QuestionsTableComponent implements OnInit {
   questions$: Observable<Question[]> | undefined;
 
   questionsList: Question[] = [];
+  questionsData: Question[] = [];
+
+  questionModel: QuestionModel = new QuestionModel();
 
   openEdit = false;
-  formValue !: FormGroup;
+  formValue!: FormGroup;
+  showAdd!: boolean;
+  showUpdate!: boolean;
 
-  constructor( private questionsLoadingService: QuestionsLoadingService,
+  constructor(
+    private questionsLoadingService: QuestionsLoadingService,
     private questionStore: Store<QuestionsState>,
     private formBuilder: FormBuilder,
-    private questionsSyncStore: QuestionsSyncService,
-    private router: Router) { }
+    private questionsSyncStore: QuestionsSyncService
+  ) {}
 
   ngOnInit() {
     this.questionsSyncStore.init();
@@ -40,8 +51,72 @@ export class QuestionsTableComponent implements OnInit {
     this.formValue = this.formBuilder.group({
       id: [''],
       text: [''],
-      type: ['']
-    })
+      type: [''],
+    });
+  }
+
+  onAddQuestion() {
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showUpdate = false;
+  }
+
+  postQuestionDetails() {
+    this.questionModel.id = this.formValue.value.id;
+    this.questionModel.text = this.formValue.value.text;
+    this.questionModel.type = this.formValue.value.type;
+
+    this.questionsLoadingService.postQuestion(this.questionModel).subscribe(
+      (res: any) => {
+        console.log(res);
+        let ref = document.getElementById('cancel');
+        ref?.click();
+        this.formValue.reset();
+        this.closeModal();
+      },
+      (error) => {
+        console.log('Something went wrong.');
+      }
+    );
+  }
+
+  getAllQuestions() {
+    this.questionsLoadingService.getQuestions().subscribe((res) => {
+      this.questionsData = res;
+    });
+  }
+
+  onDeleteQuestion(question: any) {
+    this.questionsLoadingService
+      .deleteQuestion(question.id)
+      .subscribe((res) => {
+        this.getAllQuestions();
+      });
+  }
+
+  onEditQuestion(question: any) {
+    this.showAdd = false;
+    this.showUpdate = true;
+
+    this.questionModel.id = question.id;
+    this.formValue.controls['id'].setValue(question.id);
+    this.formValue.controls['text'].setValue(question.text);
+    this.formValue.controls['type'].setValue(question.type);
+  }
+
+  onUpdateQuestionDetails() {
+    this.questionModel.id = this.formValue.value.id;
+    this.questionModel.text = this.formValue.value.text;
+    this.questionModel.type = this.formValue.value.type;
+
+    this.questionsLoadingService
+      .updateQuestion(this.questionModel, this.questionModel.id)
+      .subscribe((res) => {
+        let ref = document.getElementById('cancel');
+        ref?.click();
+        this.formValue.reset();
+        this.closeModal();
+      });
   }
 
   openModal() {
@@ -54,5 +129,6 @@ export class QuestionsTableComponent implements OnInit {
     let modal = document.getElementById('modal');
     modal?.classList.remove('modal-open');
     modal?.classList.add('modal-close');
+    this.formValue.reset();
   }
 }
