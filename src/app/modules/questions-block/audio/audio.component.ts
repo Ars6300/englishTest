ea5d95d3b1aa8727ebc35e5c4f5c9ece30855f43
DobrovskayaElena <lenayalena@tut.by>
2021-07-29 19:14:13 +0300

    refactor: audio input

--- src/app/modules/questions-block/audio/audio.component.ts
+++ src/app/modules/questions-block/audio/audio.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-audio',
   templateUrl: './audio.component.html',
   styleUrls: ['./audio.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AudioComponent implements OnInit {
   audio!: HTMLAudioElement;
 
+  audioPlay: any;
+
   disabledAudio: boolean = false;
 
-  play = false;
+  /* play = false; */
 
   tryCount: number = 0;
 
+  blobUrl: string = 'https://zvukogram.com/index.php?r=site/download&id=38307';
+
+  audioTransition: any;
+
   constructor() {}
 
-  playAudio() {
+  /*   playAudio() {
     this.play = true;
     this.audio = new Audio(
       'https://zvukogram.com/index.php?r=site/download&id=38307'
@@ -37,8 +44,16 @@ export class AudioComponent implements OnInit {
     return (
       Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
     );
-  }
+  } */
 
   ngOnInit() {
+    this.audioPlay = document.getElementById('audio');
+    this.audioPlay.shadowRoot.querySelector("input[aria-label='pause']").setAttribute("visibility", "hidden");
+    console.log(this.audioPlay);
+  }
+
+  onCheckIsPlaying(element: HTMLElement | null) {
+    element = document.getElementById('audio');
   }
 }
+
