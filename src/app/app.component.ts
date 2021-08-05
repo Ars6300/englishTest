39453ddc6f722d115d1b36f17e0c5a729051139b
Arslan Tadjiev <ars6300@live.com>
2021-08-06 03:17:48 +0500

    fix: remove language lag on page refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { StorageService } from './core/services/storage.service';
+import { LanguageService } from './core/services/language.service';
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from 'src/environments/environment';
@@ -8,8 +10,18 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private translateService: TranslateService) {}
+  constructor(
+    private translateService: TranslateService,
+    private languageService: LanguageService,
+    private storage: StorageService
+  ) {}
   ngOnInit(): void {
-    this.translateService.use(environment.defaultLocale);
+    const lang: string | null = this.storage.getItem('lang');
+    if (lang) {
+      this.languageService.changeLanguage(lang);
+    } else {
+      this.storage.setItem('lang', environment.defaultLocale);
+      this.translateService.use(environment.defaultLocale);
+    }
   }
 }
--- src/app/shared/components/language-select/language-select.component.ts
+++ src/app/shared/components/language-select/language-select.component.ts
@@ -1,3 +1,4 @@
+import { LanguageService } from './../../../core/services/language.service';
 import { environment } from 'src/environments/environment';
 import { StorageService } from './../../../core/services/storage.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,45 +11,11 @@ import { LANGUAGE_ENG, LANGUAGE_RU } from './language-select.constants';
   styleUrls: ['./language-select.component.scss'],
 })
 export class LanguageSelectComponent implements OnInit {
-  obj: any[] = [
-    {
-      langText: LANGUAGE_ENG,
-      selected: true,
-      lang: 'en',
-    },
-    {
-      langText: LANGUAGE_RU,
-      selected: false,
-      lang: 'ru',
-    },
-  ];
-
-  ngOnInit(): void {
-    const lang: string | null = this.storage.getItem('lang');
-    if (lang) {
-      this.changeLanguage(lang);
-    } else {
-      this.storage.setItem('lang', environment.defaultLocale);
-    }
-  }
-
-  changeLanguage(lang: string) {
-    this.obj = this.obj.map((item) => {
-      if (item.lang === lang) {
-        return { ...item, selected: true };
-      }
-      this.translate.use(lang);
-      this.storage.setItem('lang', lang);
-      return { ...item, selected: false };
-    });
-  }
+  ngOnInit(): void {}
 
   onLanguageSelect(target: string) {
-    this.changeLanguage(target);
+    this.languageService.changeLanguage(target);
   }
 
-  constructor(
-    private translate: TranslateService,
-    private storage: StorageService
-  ) {}
+  constructor(public languageService: LanguageService) {}
 }
