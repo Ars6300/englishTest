a384b9524bf927e573f114d7d77cda64b99135b0
Arslan Tadjiev <ars6300@live.com>
2021-08-17 19:04:59 +0500

    fix: uncomment http service

--- src/app/core/services/results/profile-results.service.ts
+++ src/app/core/services/results/profile-results.service.ts
@@ -10,24 +10,9 @@ import { environment } from 'src/environments/environment';
 export class ProfileResultsService {
   constructor(private http: HttpClient) {}
 
-  // getAllResults(userId: string | null): Observable<any> {
-  //   return this.http.get(
-  //     `${environment.api_URL}/api/test/history?userId=${userId}`
-  //   );
-  // }
-
   getAllResults(userId: string | null): Observable<any> {
-    return of([
-      {
-        examDate: '12.21.12',
-        level: 'A1',
-        status: 'completed',
-        grammarMark: 8,
-        auditionMark: 2,
-        writingMark: 3,
-        speakingMark: 4,
-        totalMark: 5,
-      },
-    ]);
+    return this.http.get(
+      `${environment.api_URL}/api/test/history?userId=${userId}`
+    );
   }
 }
